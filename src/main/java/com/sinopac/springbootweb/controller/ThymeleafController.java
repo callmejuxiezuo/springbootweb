7f6c70f661b57847cef135f75740de04e67e5a1a
hanpeng <33281509+callmejuxiezuo@users.noreply.github.com>
2020-08-31 21:10:13 +0800

    Thymeleaf 条件判断，list/map迭代，迭代器的状态变量，3个作用域，URL表达式

--- src/main/java/com/sinopac/springbootweb/controller/ThymeleafController.java
+++ src/main/java/com/sinopac/springbootweb/controller/ThymeleafController.java
@@ -1,10 +1,12 @@
 package com.sinopac.springbootweb.controller;
 
+import com.sinopac.springbootweb.pojo.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 /**
  * Thymeleaf页面跳转
@@ -12,10 +14,34 @@ import java.util.Date;
  */
 @Controller
 public class ThymeleafController {
-    @GetMapping("thymeleaf")
-    public  String showThymeleaf(Model model) {
+    @GetMapping("/thymeleaf")
+    public  String showThymeleaf(Model model, HttpServletRequest request) {
         model.addAttribute("Thymeleaf", "showThymeleaf");
         model.addAttribute("date", new Date());
+        model.addAttribute("age", "22");
+
+        // 迭代遍历List
+        List<Users> showUsersList = new ArrayList<Users>();
+        Users userOne = new Users("ZhangSan", 23);
+        Users userTwo = new Users("LiSi", 22);
+        Users userThree = new Users("王五", 21);
+        showUsersList.add(userOne);
+        showUsersList.add(userTwo);
+        showUsersList.add(userThree);
+        model.addAttribute("usersList", showUsersList);
+
+        // 迭代遍历Map
+        Map<String, Users> userMap = new HashMap<>(3);
+        userMap.put("userOne", userOne);
+        userMap.put("userTwo", userTwo);
+        userMap.put("userThree", userThree);
+        model.addAttribute("userMap", userMap);
+
+        //域对象
+        request.setAttribute("request", "request");
+        request.getSession().setAttribute("session", "session");
+        request.getSession().getServletContext().setAttribute("servletContext", "servletContext");
+
         return "thymeleaf";
     }
 }
