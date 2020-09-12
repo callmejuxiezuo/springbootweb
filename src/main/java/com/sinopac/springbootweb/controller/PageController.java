965211c38eb6b8add437df4ab57635e90d873c96
callmejuxiezuo <1013526188@qq.com>
2020-09-12 18:15:53 +0800

    springbootweb 页面跳转增加对象至Model

---
+++ src/main/java/com/sinopac/springbootweb/controller/PageController.java
@@ -0,0 +1,31 @@
+package com.sinopac.springbootweb.controller;
+
+import com.sinopac.springbootweb.pojo.Users;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author hp
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){
+
+        return page;
+    }
+
+    /**
+     * 跳转到含有页面对象的page,通过给对象users,springMVC会自动传入到Model
+     * @param page
+     * @param users
+     * @return
+     */
+    @RequestMapping("/{page}")
+    public String showPage2(@PathVariable String page, Users users){
+
+        return page;
+    }
+}
